2d471a0a4afbd39afb8130811831210ed9af3de1
Greg Tatum <tatum.creative@gmail.com>
2013-11-22 00:27:04 -0600

    Added energy costs and new phenome class

--- js/ui.js
+++ js/ui.js
@@ -3,8 +3,9 @@ Evo.UI = (function() {
 	var self = function() {
 		
 		this.$sectionNavigator = $('.section-navigator');
+		this.$backButton = $('.back-button');
 		this.$sectionNavigator.find('input[type=button]').on('mouseup', this.sectionNavigatorHandler.bind(this));
-		$('.section-go-back').on('mouseup', this.exitSectionHandler.bind(this));
+		$('.back-button-input').on('mouseup', this.exitSectionHandler.bind(this));
 	};
 	
 	self.prototype = {
@@ -16,6 +17,7 @@ Evo.UI = (function() {
 			var id = $(e.target).attr('data-id-show');
 			if(id) {
 				$("#"+id).addClass('show');
+				this.$backButton.addClass('show');
 				this.$sectionNavigator.addClass('hide');
 			}
 		},
@@ -23,8 +25,94 @@ Evo.UI = (function() {
 		exitSectionHandler : function(e) {
 			console.log('hiding section');
 			if(e) e.preventDefault();
-			$(e.target).parents('fieldset').removeClass('show');
+			$('fieldset').removeClass('show');
+			this.$backButton.removeClass('show');
 			this.$sectionNavigator.removeClass('hide');
+		}
+	};
+	
+	return self;
+})();
+
+Evo.Binding = (function() {
+	
+	/*
+	 * Model / View binding
+	 * JS Object / DOM Element binding
+	 */
+	
+	var self = function($view, getter, setter) {
+		this.$view = $view;
+		this.getter = getter;
+		this.setter = setter;
+		
+		this.$view.on('change.Evo-Inputs', this.updateModel.bind(this));
+		this.updateView();
+	};
+	
+	//Public Methods
+	self.prototype = {
+		
+		getViewValue : function() {
+			
+			if(this.$view[0] === undefined) return;
+
+			//Radio input
+			if(this.$view.is("input[type='radio']")) {
+				return this.$view.is(':checked').val();
+
+			//Standard input
+			} else if (this.$view.is("input")) {
+				return this.$view.val();
+
+			//DOM Element
+			} else {
+				return this.$view.html();
+			}
+			
+		},
+		
+		updateModel : function() {
+			
+			var requestedValue, setValue;
+			
+			
+			//Get the requested value change
+			requestedValue = this.getViewValue();
+			
+			//Send it to the setter, which should return the value actually set
+			this.setter(requestedValue);
+			
+			//Update the input with the set value
+			this.updateView();
+		},
+				
+		updateView : function() {
+			
+			var value = this.getter();
+			
+			if(this.$view[0] === undefined) return;
+
+			//Radio input
+			if(this.$view.is("input[type='radio']")) {
+				//TODO this.$view.is(':checked').val();
+
+			//Standard input
+			} else if (this.$view.is("input")) {
+				this.$view.val(value);
+
+			//DOM Element
+			} else {
+				this.$view.html(value);
+			}
+			
+		},
+				
+		remove : function() {
+			this.$view = null;
+			this.getter = null;
+			this.setter = null;
+			this.$view.off('change.Evo-Inputs', this.updateModel);
 		}
 	};
 	
